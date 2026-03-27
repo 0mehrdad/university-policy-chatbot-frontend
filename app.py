b0d05e61fffb46f70c320e58bfd1b7d0212f5ff3
0mehrdad <0madam56@solent.ac.uk>
2026-03-27 19:39:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import requests
+import os
 
-API_URL = "http://127.0.0.1:8000/ask"
+API_URL = 'http://University-chat-bot-env-1.eba-znhr7bta.us-east-1.elasticbeanstalk.com/ask'
 
-st.set_page_config(page_title="University Policy Assistant", page_icon="📚")
-
-st.title("📚 University of London Policy Assistant")
+st.set_page_config(page_title="Policy Assistant", page_icon="📚")
+st.title("📚 University Policy Assistant")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
@@ -16,23 +16,33 @@ for msg in st.session_state["messages"]:
 
 if query := st.chat_input("Ask a question..."):
     st.session_state["messages"].append({"role": "user", "content": query})
+
     with st.chat_message("user"):
         st.markdown(query)
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            r = requests.post(API_URL, json={"question": query})
-            data = r.json()
+            try:
+                r = requests.post(
+                    API_URL,
+                    json={"question": query},
+                    timeout=10
+                )
 
-            answer = data["answer"]
-            sources = data.get("sources", [])
+                r.raise_for_status()
+                data = r.json()
 
-            source_text = "\n".join([f"- {s}" for s in sources])
-            if sources:
-                response = f"**Answer:** {answer}\n\n**Sources:**\n{source_text}"
-            else:
+                answer = data.get("answer", "No answer")
+                sources = data.get("sources", [])
+
+                source_text = "\n".join([f"- {s}" for s in sources])
                 response = f"**Answer:** {answer}"
 
-            st.markdown(response)
+                if sources:
+                    response += f"\n\n**Sources:**\n{source_text}"
 
+            except Exception as e:
+                response = "⚠️ Error: Unable to get response from server."
+
+    st.markdown(response)
     st.session_state["messages"].append({"role": "assistant", "content": response})
